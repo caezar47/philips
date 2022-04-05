b29e9b2124b3a7e5f6a98360eca1d2066b69230b
Сергей Иванов <s.ivanov@aristos.pw>
2022-04-05 15:51:54 +0300

    P-8766 :: Ультрафиолетовый дезинфектор

---
+++ src/js/button.js
@@ -0,0 +1,7 @@
+;(function ($, window, document, undefined) {
+	$(document).ready(function() {
+		$(".button_in-stock").each(function () {
+			$(this).text('Купить');
+		});
+	});
+})(jQuery, window, document);
--- src/js/scrollto.js
+++ src/js/scrollto.js
@@ -1,26 +1,36 @@
 ;(function ($, window, document, undefined) {
 	$(document).ready(function() {
-		var header = $('.header-bottom');
 		var header_size = 0;
 		var scrollBtn = $('[data-scrollto]');
 		var hash = window.location.hash;
 		var hashId = hash.replace('#', '');
+		var scrollDiff = 0;
+		var scrollSpeed = 600;
 
-		if (header.length > 0) {
-			header_size = header.outerHeight();
-		}
+		$(window).scroll(function() {
+			var header = $('.header-fixed .header-bottom');
+			if (header.length > 0) {
+				header_size = header.outerHeight();
+			}
+		}).scroll();
+
 		if (scrollBtn.length > 0) {
 			scrollBtn.on('click', function (e) {
 				e.preventDefault();
 				var btn = $(this);
 				var block = $(btn.attr('href')).eq(0);
-				var diff = parseInt(btn.attr('data-scrollto-diff')) || 0;
-				var speed = parseInt(btn.attr('data-scrollto-speed')) || 777;
+				var diff = parseInt(btn.attr('data-scrollto-diff')) || scrollDiff;
+				var speed = parseInt(btn.attr('data-scrollto-speed')) || scrollSpeed;
 				$('body, html').animate({
 					scrollTop: (block.offset().top - header_size + diff)
 				}, speed);
 				return false;
 			});
+		}
+		if (hash) {
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - header_size + scrollDiff
+			}, scrollSpeed);
 		}
 	});
 })(jQuery, window, document);
