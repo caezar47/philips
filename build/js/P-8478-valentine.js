6287f4250c1eb24f71ec1de82cadf0ae30b39859
Сергей Иванов <s.ivanov@aristos.pw>
2022-07-06 09:20:02 +0300

    P-9054, P-9068

--- build/js/P-8478-valentine.js
+++ build/js/P-8478-valentine.js
@@ -1,162 +1,162 @@
 ; (function($, window, document, undefined) {
-    $(document).on('ready', function(e) {
-        var hash = window.location.hash;
-        var hashId = hash.replace('#', '');
-        if( hash ){
-            $('.valentine-card__category .filter__checkbox.cat').removeClass('is-active');
-        }
-        $('.valentine-card__category .filter__checkbox.cat[data-link="'+hash+'"]').addClass('is-active');
-
-        if (hash  == '#for-woman' || hash  == '#for-men') {
-            $('.all_categories.cat-block').addClass('hide');
-            $('.all_categories[id="'+hashId+'"]').removeClass('hide');
-            $('html, body').animate({
-                scrollTop: $('.valentine-card__flex').offset().top - 60
-            }, 300);
-        }
-
-        var resetButton = document.querySelector('.filters__reset');
-        var resetButton2 = document.querySelector('.filters__reset2');
-        var filterBtn = document.querySelector('.filter-outter');
-
-        filterBtn.addEventListener('click', function(e) {
-            if(e.target.classList.contains('filter-outter') || e.target.classList.contains('filter-outter-img') || e.target.classList.contains('filter-outter-text')) {
-                filterBtn.querySelector(".filter").classList.toggle('block');
-            }
-        });
-
-        var showList = function(event) {
-            var checkbox = event.target;
-            document.querySelectorAll('.filter__checkbox.cat').forEach(function(item) {
-                var target = document.querySelector(item.dataset.link);
-                if (!item.checked) {
-                    item.classList.remove('is-active');
-                    target.classList.add('hide');
-                } else {
-                    $('.filter__checkbox.cat').removeClass('is-active').prop('checked', false);
-                    $('.all_categories.cat-block').addClass('hide');
-                    item.classList.add('is-active');
-                    target.classList.remove('hide');
-
-                }
-            });
-        };
-
-        var showSaleList = function (event) {
-            if(window.innerWidth > 1024) {
-
-                document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale').forEach(function (item) {
-
-                    var saleTarget = document.querySelectorAll(item.dataset.saleLink);
-                    if (!item.checked) {
-                        item.classList.remove('is-active');
-                        saleTarget.forEach(function (item) {
-                            item.classList.add('hide');
-                        })
-                    } else {
-                        item.classList.add('is-active');
-                        saleTarget.forEach(function (item) {
-                            if(item.innerHTML !== "") {
-                                item.classList.remove('hide');
-                            }
-                            else {
-                                item.closest('.all_categories').classList.add('hide');
-                            }
-                        })
-
-                    }
-                });
-                if (document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale.is-active').length === 0) {
-                    resetSaleFilters();
-                }
-            }
-
-            else {
-                document.querySelectorAll('.filter__checkbox.sale.mob').forEach(function (item) {
-                    var saleTarget = document.querySelectorAll(item.dataset.saleLink);
-                    if (!item.checked) {
-                        item.classList.remove('is-active');
-                        saleTarget.forEach(function (item) {
-                            item.classList.add('hide');
-                        })
-                    } else {
-                        item.classList.add('is-active');
-                        saleTarget.forEach(function (item) {
-                            if(item.innerHTML !== "") {
-                                item.classList.remove('hide');
-                            }
-                            else {
-                                item.closest('.all_categories').classList.add('hide');
-                            }
-                        })
-
-                    }
-                });
-                if (document.querySelectorAll('.filter__checkbox.sale.mob.is-active').length === 0) {
-                    resetSaleFilters();
-                }
-            }
+	$(document).on('ready', function(e) {
+		var hash = window.location.hash;
+		var hashId = hash.replace('#', '');
+		if( hash ){
+			$('.valentine-card__category .filter__checkbox.cat').removeClass('is-active');
+		}
+		$('.valentine-card__category .filter__checkbox.cat[data-link="'+hash+'"]').addClass('is-active');
+		$('.all_categories.cat-block').addClass('hide');
+		$('.all_categories[id="'+hashId+'"]').removeClass('hide');
+		if (hash  == '#for-woman' || hash  == '#for-men') {
+			$('html, body').animate({
+				scrollTop: $('.valentine-card__flex').offset().top - 60
+			}, 300);
+		}  // Якорь до блока тест драйв при открытие страницы
 
 
-        }
+		var resetButton = document.querySelector('.filters__reset');
+		var resetButton2 = document.querySelector('.filters__reset2');
+		var filterBtn = document.querySelector('.filter-outter');
 
-        document.querySelectorAll('.valentine-card__filter-mob .filter__checkbox.sale.mob').forEach(function(item) {
-            item.addEventListener('change', showSaleList);
-        });
+		filterBtn.addEventListener('click', function(e) {
+			if(e.target.classList.contains('filter-outter') || e.target.classList.contains('filter-outter-img') || e.target.classList.contains('filter-outter-text')) {
+				filterBtn.querySelector(".filter").classList.toggle('block');
+			}
+		});
 
-        document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale').forEach(function(item) {
-            item.addEventListener('change', showSaleList);
-        });
+		var showList = function(event) {
+			var checkbox = event.target;
+			document.querySelectorAll('.filter__checkbox.cat').forEach(function(item) {
+				var target = document.querySelector(item.dataset.link);
+				if (!item.checked) {
+					item.classList.remove('is-active');
+					target.classList.add('hide');
+				} else {
+					$('.filter__checkbox.cat').removeClass('is-active').prop('checked', false);
+					$('.all_categories.cat-block').addClass('hide');
+					item.classList.add('is-active');
+					target.classList.remove('hide');
 
-        document.querySelectorAll('.valentine-card__category .filter__checkbox.cat').forEach(function(item) {
-            item.addEventListener('change', showList);
-        });
+				}
+			});
+		};
 
-        resetButton.addEventListener('click', function(e) {
-            resetSaleFilters();
-        })
-        resetButton2.addEventListener('click', function(e) {
-            resetSaleFilters();
-        })
+		var showSaleList = function (event) {
+			if(window.innerWidth > 1024) {
 
-        function resetSaleFilters () {
-            document.querySelectorAll('.container_widgets .all_categories .sale-block').forEach(function(item) {
-                if (item.classList.contains('hide')) {
-                    item.classList.remove('hide');
-                }
-            });
-            document.querySelectorAll('.filter__checkbox.sale').forEach(function(item) {
-                if(item.classList.contains('is-active')) {
-                    item.checked = false;
-                    item.classList.remove('is-active');
-                }
-            });
-        }
+				document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale').forEach(function (item) {
 
-        var show_more_btns = Array.prototype.slice.call(document.querySelectorAll('.widget-show__more'));
-        show_more_btns.forEach(function(item, i) {
-            item.addEventListener('click', function(e) {
-                e.preventDefault();
-                e.target.closest('.all_categories').setAttribute("data-reduced", "false");
-            });
-        });
+					var saleTarget = document.querySelectorAll(item.dataset.saleLink);
+					if (!item.checked) {
+						item.classList.remove('is-active');
+						saleTarget.forEach(function (item) {
+							item.classList.add('hide');
+						})
+					} else {
+						item.classList.add('is-active');
+						saleTarget.forEach(function (item) {
+							if(item.innerHTML !== "") {
+								item.classList.remove('hide');
+							}
+							else {
+								item.closest('.all_categories').classList.add('hide');
+							}
+						})
 
-        var toTop = $('#promo #to-top');
-        var filterBlock = $('.valentine-card__filter-wrap');
-        $(window).scroll (function () {
-            if ($(this).scrollTop () > 850) {
-                toTop.addClass('is-visible');
-            } else {
-                toTop.removeClass('is-visible');
-            }
-        });
-        toTop.on('click', function(e){
-            e.preventDefault();
-            $('body, html').animate({
-                scrollTop: (filterBlock.offset().top - 100)
-            }, 800);
-            return false;
-        });
+					}
+				});
+				if (document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale.is-active').length === 0) {
+					resetSaleFilters();
+				}
+			}
 
-    });
+			else {
+				document.querySelectorAll('.filter__checkbox.sale.mob').forEach(function (item) {
+					var saleTarget = document.querySelectorAll(item.dataset.saleLink);
+					if (!item.checked) {
+						item.classList.remove('is-active');
+						saleTarget.forEach(function (item) {
+							item.classList.add('hide');
+						})
+					} else {
+						item.classList.add('is-active');
+						saleTarget.forEach(function (item) {
+							if(item.innerHTML !== "") {
+								item.classList.remove('hide');
+							}
+							else {
+								item.closest('.all_categories').classList.add('hide');
+							}
+						})
+
+					}
+				});
+				if (document.querySelectorAll('.filter__checkbox.sale.mob.is-active').length === 0) {
+					resetSaleFilters();
+				}
+			}
+
+
+		}
+
+		document.querySelectorAll('.valentine-card__filter-mob .filter__checkbox.sale.mob').forEach(function(item) {
+			item.addEventListener('change', showSaleList);
+		});
+
+		document.querySelectorAll('.valentine-card__filter .filter__checkbox.sale').forEach(function(item) {
+			item.addEventListener('change', showSaleList);
+		});
+
+		document.querySelectorAll('.valentine-card__category .filter__checkbox.cat').forEach(function(item) {
+			item.addEventListener('change', showList);
+		});
+
+		resetButton.addEventListener('click', function(e) {
+			resetSaleFilters();
+		})
+		resetButton2.addEventListener('click', function(e) {
+			resetSaleFilters();
+		})
+
+		function resetSaleFilters () {
+			document.querySelectorAll('.container_widgets .all_categories .sale-block').forEach(function(item) {
+				if (item.classList.contains('hide')) {
+					item.classList.remove('hide');
+				}
+			});
+			document.querySelectorAll('.filter__checkbox.sale').forEach(function(item) {
+				if(item.classList.contains('is-active')) {
+					item.checked = false;
+					item.classList.remove('is-active');
+				}
+			});
+		}
+
+		var show_more_btns = Array.prototype.slice.call(document.querySelectorAll('.widget-show__more'));
+		show_more_btns.forEach(function(item, i) {
+			item.addEventListener('click', function(e) {
+				e.preventDefault();
+				e.target.closest('.all_categories').setAttribute("data-reduced", "false");
+			});
+		});
+
+		var toTop = $('#promo #to-top');
+		var filterBlock = $('.valentine-card__filter-wrap');
+		$(window).scroll (function () {
+			if ($(this).scrollTop () > 850) {
+				toTop.addClass('is-visible');
+			} else {
+				toTop.removeClass('is-visible');
+			}
+		});
+		toTop.on('click', function(e){
+			e.preventDefault();
+			$('body, html').animate({
+				scrollTop: (filterBlock.offset().top - 100)
+			}, 800);
+			return false;
+		});
+
+	});
 })(jQuery, window, document);
---
+++ build/js/P-9054-avent.js
@@ -0,0 +1,195 @@
+;
+(function($, window, document, undefined) {
+	$(document).ready(function() {
+
+		let active = ['all_sales', 'all_categories'];
+		let patterns = {
+			category: /cat+/,
+			sale: /sale+/
+		}
+
+		let show_filters = document.querySelector('.manage_filters');
+		let filters = document.querySelector('.filter');
+		let filtersWrapper = document.querySelector('.filter_wrapper');
+		let goods = document.querySelector('.goods .container_widgets').children // all the widgets
+		let checkboxes = document.querySelectorAll('.filter__checkbox');
+		//let resetAll = document.querySelector('.reset__button');
+
+		show_filters.addEventListener('click', () => {
+			filtersWrapper.classList.toggle('hide');
+			show_filters.classList.toggle('reverse');
+			filters.classList.toggle('unactive')
+		})
+
+		// resetAll.addEventListener('click', () => {
+		//   checkboxes.forEach((item, i) => {
+		//     item.checked = false;
+		//   });
+		//   active = ['all_sales', 'all_categories'];
+		//   printWidgets();
+		// })
+
+		checkboxes.forEach((item, i) => {
+			item.addEventListener('change', () => {
+				refresh();
+				scrollTop();
+				if (active.length == 0) {
+					active = ['all_sales', 'all_categories'];
+				}
+				printWidgets();
+			})
+		});
+
+		function refresh() {
+			active.length = 0;
+			checkboxes.forEach((elem, i) => {
+				if (elem.checked === true) {
+					active.push(elem.getAttribute('id'))
+				}
+			});
+
+			// functions to check if the array already contains category or/and sale
+
+			function isCat(item) {
+				return patterns.category.test(item);
+			}
+
+			function isSale(item) {
+				return patterns.sale.test(item);
+			}
+
+			// check if indeed contain any of them
+
+			if (!active.some(isSale) && !active.some(isCat)) {
+				active.push('all_sales');
+				active.push('all_categories');
+			}
+
+			if (!active.some(isCat)) {
+				active.push('all_categories');
+			}
+
+			if (!active.some(isSale)) {
+				active.push('all_sales');
+			}
+		}
+
+		function printWidgets(attr) {
+			for (let i = 0; i < goods.length; i++) { // clears all the previous filters
+				goods[i].classList.remove('hide');
+			}
+
+			active = active.sort().reverse()
+
+			let categories = [];
+			categories.length = 0;
+			let sales = [];
+			sales.length = 0;
+
+			for (let i = 0; i < active.length; i++) {
+				if (patterns.category.test(active[i])) {
+					categories.push(active[i])
+				} else {
+					sales.push(active[i])
+				}
+			}
+
+			if (active[0] == 'all_sales'){
+				active.shift()
+
+			}
+
+			categories.length == 0 ? categories.push('all_categories') : categories
+			sales.length == 0 ? sales.push('all_sales') : sales
+			active.length = 0
+
+			if (sales[0] == 'all_sales'){
+				sales.shift()
+				sales.unshift('sale_1','sale_2','sale_3','sale_4','sale_5','sale_6','sale_7')
+			}
+
+			for (let i = 0; i < sales.length; i++) {
+				for (let j = 0; j < categories.length; j++) {
+					let curr = sales[i] + ' ' + categories[j];
+					active.push(curr);
+				}
+			}
+
+			for (let i = 0; i < goods.length; i++) {
+				goods[i].classList.remove('show');
+				goods[i].classList.add('hide');
+			}
+
+			for (let i = 0; i < goods.length; i++) {
+
+				// delete hode class
+				let cur_class = goods[i].getAttribute('class').split(' ');
+				cur_class.length = cur_class.length - 1
+				cur_class = cur_class.join(' ')
+
+				for (let j = 0; j < active.length; j++) {
+					if (active[j] == cur_class) {
+						goods[i].classList.remove('hide');
+					}
+				}
+			}
+		}
+
+		// making the filter fixed
+		sale_filter = document.querySelector('.sales')
+		category_filter = document.querySelector('.category')
+		filter_wrapper = document.querySelector('.filter_wrapper')
+		let filters_height = filters.offsetHeight;
+
+		window.addEventListener('scroll', (e) => {
+
+			if (window.scrollY >= 700 && window.scrollY <= filters.offsetHeight + 130) { // виджеты
+				filter_wrapper.classList.add('fixed');
+				filters.classList.remove('flex-end');
+				filters_height = filters.offsetHeight;
+				filters.classList.remove('flex-end');
+			} else if (window.scrollY <= 700 && window.scrollY <= filters.offsetHeight + 130){ // не долистал до виджетов
+				filter_wrapper.classList.remove('fixed');
+				filters.classList.remove('flex-end');
+				filters_height = filters.offsetHeight;
+			} else { // пролистал виджеты
+				filters.classList.add('flex-end');
+				filter_wrapper.classList.remove('fixed');
+				filters_height = filters.offsetHeight;
+			}
+
+		})
+
+		function scrollTop (){
+			if (window.screen.width > 600){
+				window.scrollTo({
+					top: 500,
+					behavior: "smooth"
+				});
+				// } else if (window.screen.width <= 991 && window.screen.width > 600) {
+				//   window.scrollTo({
+				//       top: 500,
+				//       behavior: "smooth"
+				//   });
+			}
+		}
+
+
+		if($(window).width() <= 769 && !(filtersWrapper.classList.contains('hide'))) {
+			filtersWrapper.classList.add('hide')
+			show_filters.classList.add('reverse')
+			filters.classList.add('unactive')
+		}
+
+		let show_more_btns = Array.prototype.slice.call(document.querySelectorAll('.widget-show__more'));
+		show_more_btns.forEach((item, i) => {
+			item.addEventListener('click', (e) => {
+				e.preventDefault();
+				e.target.closest('.all_categories').setAttribute("data-reduced", "false");
+			})
+		});
+
+
+
+	});
+})(jQuery, window, document);
